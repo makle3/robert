76cab3e309524d6ede7131c76ea226c6ea399f26
makle3 <marras11@student.aau.dk>
2018-06-10 18:45:20 +0200

    a

--- Bot.py
+++ Bot.py
@@ -17,14 +17,14 @@ async def my_background_task():
     await client.wait_until_ready()
     timeToRelease = datetime.datetime.strptime("August 14 2018", '%B %d %Y')
     while not client.is_closed:
-        timeNow = datetime.datetime.now()
+        timeNow = datetime.datetime.utcnow()
         delta = timeToRelease - timeNow
         hours, remainder = divmod(delta.seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
         countdown ="BFA RELEASE IN " + str(delta.days) + " DAYS, " + str(hours) + " HOURS AND " + str('%02d' % minutes) + " MINUTES! (ish)"
 
         await client.edit_channel(client.get_channel(id=os.environ['channel']), topic=countdown)
-        await asyncio.sleep(10) # task runs every 60 seconds
+        await asyncio.sleep(60) # task runs every 60 seconds
 
 
 client.loop.create_task(my_background_task())
