cea4703f3125c67a6119361dc96404709af1264d
makle3 <marras11@student.aau.dk>
2018-08-13 17:41:59 +0200

    a

--- Bot.py
+++ Bot.py
@@ -20,11 +20,13 @@ async def my_background_task():
     timeToRelease = datetime.datetime.strptime("August 14 2018", '%B %d %Y')
     while not client.is_closed:
         timeNow = datetime.datetime.utcnow() + datetime.timedelta(hours=2)
-        delta = timeToRelease - timeNow
-        hours, remainder = divmod(delta.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        countdown ="BFA RELEASE IN " + str(delta.days) + " DAYS, " + str(hours) + " HOURS AND " + str('%02d' % minutes) + " MINUTES! (ish)"
-        print(timeNow)
+        if timeNow.time() < timeToRelease.time():
+            countdown = "Ingen tid at spilde, nu skal der spilles!"
+        else:
+            delta = timeToRelease - timeNow
+            hours, remainder = divmod(delta.seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            countdown ="BFA RELEASE IN " + str(delta.days) + " DAYS, " + str(hours) + " HOURS AND " + str('%02d' % minutes) + " MINUTES! (ish)"
         try:
             await client.edit_channel(client.get_channel(id=os.environ['channel']), topic=countdown)
         except:
